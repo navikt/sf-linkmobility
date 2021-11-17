ac0b88f3184546d3d08d125dddbe9e8d4d9c476d
Björn Hägglund <bjornhagglund@Bjorns-MacBook-Pro.local>
2021-11-17 16:39:40 +0100

    test auth token with ktor

--- src/Application.kt
+++ src/Application.kt
@@ -1,5 +1,6 @@
 package no.nav.sf.linkmobility
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
@@ -7,11 +8,16 @@ import io.ktor.auth.Authentication
 import io.ktor.auth.UserIdPrincipal
 import io.ktor.auth.authenticate
 import io.ktor.auth.basic
+import io.ktor.client.HttpClient
+import io.ktor.client.request.forms.submitForm
+import io.ktor.client.statement.HttpStatement
+import io.ktor.client.statement.readBytes
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.origin
 import io.ktor.gson.gson
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.Parameters
 import io.ktor.http.content.defaultResource
 import io.ktor.http.content.resources
 import io.ktor.http.content.static
@@ -22,7 +28,12 @@ import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.prometheus.client.CollectorRegistry
 import io.prometheus.client.exporter.common.TextFormat
+import java.security.KeyStore
+import java.security.PrivateKey
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
+import token.TokenResponse
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -77,6 +88,10 @@ fun Application.module(testing: Boolean = false) {
                 TextFormat.write004(this, collectorRegistry.filteredMetricFamilySamples(names))
             }
         }
+        get("api/registersms") {
+            val res = fetchAccessTokenAndInstanceUrl()
+            call.respond(HttpStatusCode.OK, "first ${res.first} second ${res.second}")
+        }
         authenticate("auth-basic") {
             get("api/ping") {
                 val headers = call.request.headers.entries().map { "${it.key} : ${it.value}" }.joinToString("\n")
@@ -121,3 +136,91 @@ fun isAuthenticated(call: ApplicationCall): Boolean {
 }
 
  */
+
+val tokenHost = System.getenv("SF_TOKENHOST")
+
+val SFClientID = System.getenv("SFClientID")
+val SFUsername = System.getenv("SFUsername")
+val keystoreB64 = System.getenv("KeystoreJKSB64")
+val keystorePassword = System.getenv("KeystorePassword")
+val privateKeyAlias = System.getenv("PrivateKeyAlias")
+val privateKeyPassword = System.getenv("PrivateKeyPassword")
+
+val objectMapper: ObjectMapper = ObjectMapper()
+
+// Spring:
+// val requestFactory = SimpleClientHttpRequestFactory()
+// Spring:
+// val restTemplate : RestTemplate
+val client = HttpClient()
+
+suspend fun fetchAccessTokenAndInstanceUrl(): Pair<String, String> {
+    val claim = JWTClaim(iss = SFClientID, aud = tokenHost, sub = SFUsername, exp = ((System.currentTimeMillis() / 1000) + 300).toString())
+    val privateKey = PrivateKeyFromBase64Store(ksB64 = keystoreB64, ksPwd = keystorePassword, pkAlias = privateKeyAlias, pkPwd = privateKeyPassword)
+    val claimWithHeaderJsonUrlSafe = "${objectMapper.writeValueAsString(JWTClaimHeader("RS256")).encodeB64UrlSafe()}.${objectMapper.writeValueAsString(claim).encodeB64UrlSafe()}"
+    val fullClaimSignature = privateKey.sign(claimWithHeaderJsonUrlSafe.toByteArray())
+
+    /*
+    val headers = HttpHeaders()
+    headers.contentType = MediaType.APPLICATION_FORM_URLENCODED
+
+    val body : MultiValueMap<String, String> = LinkedMultiValueMap()
+    body.add("grant_type", "urn:ietf:params:oauth:grant-type:jwt-bearer")
+    body.add("assertion", "$claimWithHeaderJsonUrlSafe.$fullClaimSignature")
+
+    val entity: HttpEntity<MultiValueMap<String, String>> = HttpEntity(body, headers)
+*/
+    for (retry in 1..4) {
+        try {
+            // val response: ResponseEntity<String> = restTemplate.exchange(tokenHost,
+            //    HttpMethod.POST, entity, String::class.java)
+
+            val statement = client.submitForm<HttpStatement>(
+                url = tokenHost,
+                formParameters = Parameters.build {
+                    append("grant_type", "urn:ietf:params:oauth:grant-type:jwt-bearer")
+                    append("assertion", "$claimWithHeaderJsonUrlSafe.$fullClaimSignature")
+                }
+            )
+
+            val response = statement.execute()
+
+            if (response.status == HttpStatusCode.OK) {
+                val accessTokenResponse = objectMapper.readValue(response.readBytes(), TokenResponse::class.java)!!
+                return Pair(accessTokenResponse.access_token, accessTokenResponse.instance_url)
+            }
+        } catch (e: Exception) {
+            log.error("Attempt to fetch access token $retry of 3 failed by ${e.message} stack: ${e.printStackTrace()}}")
+            runBlocking { delay(retry * 1000L) }
+        }
+    }
+    return Pair("", "")
+}
+
+fun PrivateKeyFromBase64Store(ksB64: String, ksPwd: String, pkAlias: String, pkPwd: String): PrivateKey {
+    return KeyStore.getInstance("JKS").apply { load(ksB64.decodeB64().inputStream(), ksPwd.toCharArray()) }.run {
+        getKey(pkAlias, pkPwd.toCharArray()) as PrivateKey
+    }
+}
+
+fun PrivateKey.sign(data: ByteArray): String {
+    return this.let { java.security.Signature.getInstance("SHA256withRSA").apply {
+        initSign(it)
+        update(data)
+    }.run {
+        sign().encodeB64()
+    } }
+}
+
+data class JWTClaim(
+    val iss: String,
+    val aud: String,
+    val sub: String,
+    val exp: String
+)
+
+data class JWTClaimHeader(val alg: String)
+
+fun ByteArray.encodeB64(): String = org.apache.commons.codec.binary.Base64.encodeBase64URLSafeString(this)
+fun String.decodeB64(): ByteArray = org.apache.commons.codec.binary.Base64.decodeBase64(this)
+fun String.encodeB64UrlSafe(): String = org.apache.commons.codec.binary.Base64.encodeBase64URLSafeString(this.toByteArray())
---
+++ src/token/TokenResponse.java
@@ -0,0 +1,61 @@
+package token;
+
+public class TokenResponse {
+    private String access_token;
+    private String scope;
+
+    @Override
+    public String toString() {
+        return "TokenResponse{" +
+                "access_token='" + access_token + '\'' +
+                ", scope='" + scope + '\'' +
+                ", instance_url='" + instance_url + '\'' +
+                ", id='" + id + '\'' +
+                ", token_type='" + token_type + '\'' +
+                '}';
+    }
+
+    private String instance_url;
+    private String id;
+    private String token_type;
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public String getInstance_url() {
+        return instance_url;
+    }
+
+    public void setInstance_url(String instance_url) {
+        this.instance_url = instance_url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getToken_type() {
+        return token_type;
+    }
+
+    public void setToken_type(String token_type) {
+        this.token_type = token_type;
+    }
+}
